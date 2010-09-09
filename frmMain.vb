25391208ea928425f955085b86064dfbd10e0667
gsantella <gsantella@southhills.edu>
2010-09-09 13:23:00 -0700

    Removed MessageBox

--- frmMain.vb
+++ frmMain.vb
@@ -1,6 +1,6 @@
 ﻿Public Class frmMain
 
     Private Sub btnDisplayAccountSummary_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDisplayAccountSummary.Click
-        MessageBox.Show("Test Commit")
+       
     End Sub
 End Class
