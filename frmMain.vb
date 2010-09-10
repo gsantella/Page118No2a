50e83d52319f99f5546e09892a2190aa6f1f2a1c
Guido Santella <gsantella@southhills.edu>
2010-09-10 16:19:54 -0400

    Class Work Changes

--- frmMain.vb
+++ frmMain.vb
@@ -1,6 +1,26 @@
 ﻿Public Class frmMain
 
     Private Sub btnDisplayAccountSummary_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDisplayAccountSummary.Click
-       
+
+        Dim srReader As IO.StreamReader = IO.File.OpenText("..\..\data.txt")
+
+        Dim strAccountNum, strPastDue, strPayments, strPurchases As String
+        Dim strFormat As String = "{0}"
+
+        While Not srReader.EndOfStream
+
+            'Read Each Record from File
+            strAccountNum = srReader.ReadLine
+            strPastDue = srReader.ReadLine
+            strPayments = srReader.ReadLine
+            strPurchases = srReader.ReadLine
+
+            'Calculate 
+
+            'Output Results in Zones
+            lstAccountSummary.Items.Add(strAccountNum & strPastDue & strPayments & strPurchases)
+
+        End While
+
     End Sub
 End Class
