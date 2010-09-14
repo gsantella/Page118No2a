5c22929d66e041b7c7f38d2edad08104fbf9c44b
gsantella <gsantella@southhills.edu>
2010-09-14 14:49:56 -0400

    Go

--- frmMain.vb
+++ frmMain.vb
@@ -5,8 +5,14 @@
         Dim srReader As IO.StreamReader = IO.File.OpenText("..\..\data.txt")
 
         Dim strAccountNum, strPastDue, strPayments, strPurchases As String
-        Dim strFormat As String = "{0}"
+		Dim dblFinanceCharges, dblCurrentAmountDue As Double
+        Dim strFormat As String = "{0,-7}{1,12}{2,12}{3,12}{4,12}{5,12}"
 
+		'Print Heading in ListBox
+		'Show Alternate Spacing to Class!
+		lstAccountSummary.Items.Add(String.Format(strFormat, "Account", "Past Due", "", "", "Finace", "Current")
+		lstAccountSummary.Items.Add(String.Format(strFormat, "Number", "Amount", "Payments", "Purchases", "Charges", "Amt Due")
+		
         While Not srReader.EndOfStream
 
             'Read Each Record from File
@@ -16,9 +22,11 @@
             strPurchases = srReader.ReadLine
 
             'Calculate 
-
+			dblFinanceCharges = Math.Round((CDbl(strPastDue) - CDbl(strPayments)) * .015, 2)
+			dblCurrentAmountDue = CDbl(strPastDue) - CDbl(strPayments) + dblFinanceCharges + CDbl(strPurchases)
+			
             'Output Results in Zones
-            lstAccountSummary.Items.Add(strAccountNum & strPastDue & strPayments & strPurchases)
+            lstAccountSummary.Items.Add(String.Format(strFormat, strAccountNum, strPastDue, strPayments, strPurchases, dblFinanceCharges.ToString, dblCurrentAmountDue.ToString)
 
         End While
 
